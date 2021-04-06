b79e099e1a19dd85ffe1833d4b3fc4d6bba701ea
Nikolay <varnikgen@yandex.ru>
2021-04-06 15:01:32 +1000

    Indian Eshop - add product from admin site

--- store/models/product.py
+++ store/models/product.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='Товар')
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    description = models.CharField(max_length=200, default='')
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    description = models.CharField(max_length=200, default='', null=True, blank=True)
+    image = models.ImageField(upload_to='uploads/products/', null=True, blank=True)
 
     def __str__(self):
         return self.name
