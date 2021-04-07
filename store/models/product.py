657000d238bd3fea74bc1941f16fc67bc78985ad
Nikolay <varnikgen@yandex.ru>
2021-04-07 15:31:45 +1000

    Indian Eshop - ForeignKey Product - Category

--- store/models/product.py
+++ store/models/product.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from .category import Category
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, verbose_name='Товар')
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     description = models.CharField(max_length=200, default='', null=True, blank=True)
