774126a42f77b6eb6eb936ff603dce703d8c4d4d
Nikolay <varnikgen@yandex.ru>
2021-04-07 15:39:10 +1000

    Indian Eshop - Table view in Admin Site

--- store/admin.py
+++ store/admin.py
@@ -4,5 +4,13 @@ from .models.category import Category
 from .models.product import Product
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'description', 'category']
+
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name', 'image']
+
+
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCategory)
