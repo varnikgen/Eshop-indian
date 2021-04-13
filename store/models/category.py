6cb1ccbb44e0a50ed0ed5e2baf78453c0991dca3
Nikolay <varnikgen@yandex.ru>
2021-04-13 15:57:44 +1000

    Indian Eshop - Listing Category In Home Page

--- store/models/category.py
+++ store/models/category.py
@@ -5,5 +5,10 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='Товар')
     image = models.ImageField(upload_to='uploads/category/', null=True, blank=True)
 
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
+
+
     def __str__(self):
         return self.name
--- store/views.py
+++ store/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models.category import Category
 from .models.product import Product
 
 
 def index(request):
     products = Product.get_all_products()
-    return render(request, 'index.html', {'products': products})
+    categories = Category.get_all_categories()
+    data = {'categories': categories, 'products': products, }
+    return render(request, 'index.html', data)
