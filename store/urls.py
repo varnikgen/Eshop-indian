659f00b40bcad7eb99d40942bd89e4b96df077a2
Nikolay <varnikgen@yandex.ru>
2021-04-26 15:39:15 +1000

    Indian Eshop - Login Page

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, signup
+from .views import index, signup, login
 
 
 urlpatterns = [
     path('', index, name='homepage'),
     path('signup', signup),
+    path('login', login),
 ]
--- store/views.py
+++ store/views.py
@@ -45,40 +45,47 @@ def validateCustomer(customer):
     
     return error_message
 
+def registerUser(request):
+    postData = request.POST
+    first_name = postData.get('firstname')
+    last_name = postData.get('lastname')
+    phone = postData.get('phone')
+    email = postData.get('email')
+    password = postData.get('password')
+
+    # Проверка
+    value = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'phone': phone,
+        'email': email,
+    }
+    error_message = None
+
+    customer = Customer(first_name=first_name, last_name=last_name,
+                        phone=phone, email=email, password=password)
+
+    error_message = validateCustomer(customer)
+
+    # Сохранение
+    if not error_message:
+        print(first_name, last_name, phone, email, password)
+        customer.password = make_password(customer.password)
+        customer.register()
+        return redirect('homepage')
+    else:
+        data = {
+            'error': error_message,
+            'values': value
+        }
+        return render(request, 'signup.html', data)
+
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html')
     else:
-        postData = request.POST
-        first_name = postData.get('firstname')
-        last_name = postData.get('lastname')
-        phone = postData.get('phone')
-        email = postData.get('email')
-        password = postData.get('password')
+        return registerUser(request)
 
-        # Проверка
-        value = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'phone': phone,
-            'email': email,
-        }
-        error_message = None
-
-        customer = Customer(first_name=first_name, last_name=last_name,
-                            phone=phone, email=email, password=password)
-
-        error_message = validateCustomer(customer)
-
-        # Сохранение
-        if not error_message:
-            print(first_name, last_name, phone, email, password)
-            customer.password = make_password(customer.password)
-            customer.register()
-            return redirect('homepage')
-        else:
-            data = {
-                'error': error_message,
-                'values': value
-            }
-            return render(request, 'signup.html', data)
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
