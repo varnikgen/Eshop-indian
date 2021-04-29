cefb4b781bd120cbf91b6f782582ad1cbe220cf8
Nikolay <varnikgen@yandex.ru>
2021-04-29 16:24:58 +1000

    Indian Eshop - Cart - part 1

--- store/urls.py
+++ store/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 
 from .views.login import Login
 from .views.signup import Signup
-from .views.home import index
+from .views.home import Index
 
 
 urlpatterns = [
-    path('', index, name='homepage'),
+    path('', Index.as_view(), name='homepage'),
     path('signup', Signup.as_view(), name='signup'),
     path('login', Login.as_view(), name='login'),
 ]
--- store/views/home.py
+++ store/views/home.py
@@ -1,23 +1,27 @@
+import django
 from django.contrib.auth.hashers import make_password
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 from store.models.category import Category
 from store.models.product import Product
 
 
-print(make_password('123'))
+class Index(View):
+    def post(self, request):
+        product = request.POST.get('product')
+        print(product)
+        return redirect('homepage')
 
+    def get(self, request):
+        products = None
+        categories = Category.get_all_categories()
 
-def index(request):
-    products = None
-    categories = Category.get_all_categories()
+        categoryID = request.GET.get('category')
+        if categoryID:
+            products = Product.get_products_by_categoryID(categoryID)
+        else:
+            products = Product.get_all_products()
 
-    categoryID = request.GET.get('category')
-    if categoryID:
-        products = Product.get_products_by_categoryID(categoryID)
-    else:
-        products = Product.get_all_products()
-
-    data = {'categories': categories, 'products': products, }
-    print('you are: ', request.session.get('email'))
-    return render(request, 'index.html', data)
+        data = {'categories': categories, 'products': products, }
+        return render(request, 'index.html', data)
