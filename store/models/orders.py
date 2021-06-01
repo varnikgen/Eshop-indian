81cf6baa7e7023702c0bae08cf59876ad9684f0f
Nikolay <varnikgen@yandex.ru>
2021-06-01 12:25:22 +1000

    Indian Eshop - Adding Order status

--- store/models/orders.py
+++ store/models/orders.py
@@ -14,13 +14,14 @@ class Order(models.Model):
     address = models.CharField(max_length=250, default='', blank=True)
     phone = models.CharField(max_length=15, default='', blank=True)
     date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.customer.__str__() + " - " + str(self.id)
 
     def placeOrder(self):
         self.save()
-    
+
     @staticmethod
     def get_orders_by_customer(customer_id):
-        return Order.objects.filter(customer=customer_id)
+        return Order.objects.filter(customer=customer_id).order_by('-date')
--- store/views/orders.py
+++ store/views/orders.py
@@ -12,5 +12,4 @@ class OrderView(View):
     def get(self, request):
         customer = request.session.get('customer')
         orders = Order.get_orders_by_customer(customer)
-        print(orders)
         return render(request, 'orders.html', {'orders': orders})
