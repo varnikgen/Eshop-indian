7412938f72ac3f2edb58736cf935352d5a6d900d
Nikolay <varnikgen@yandex.ru>
2021-06-03 10:58:49 +1000

    Indian Eshop - Return Url

--- store/middlewares/auth.py
+++ store/middlewares/auth.py
@@ -4,9 +4,10 @@ from django.shortcuts import redirect
 def auth_middleware(get_response):
 
     def middleware(request):
+        return_url = request.META['PATH_INFO']
         customer = request.session.get('customer')
         if not customer:
-            return redirect('login')
+            return redirect(f'login?return_url={return_url}')
 
         response = get_response(request)
         return response
--- store/views/login.py
+++ store/views/login.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.hashers import check_password
 from django.views import View
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, HttpResponseRedirect
 
 from store.models.customer import Customer
 
 
 class Login(View):
+    return_url = None
+
     def get(self, request):
+        Login.return_url = request.GET.get('return_url')
         return render(request, 'login.html')
 
     def post(self, request):
@@ -19,7 +22,11 @@ class Login(View):
 
                 request.session['customer'] = customer.id
                 
-                return redirect('homepage')
+                if Login.return_url:
+                    return HttpResponseRedirect(Login.return_url)
+                else:
+                    Login.return_url = None
+                    return redirect('homepage')
             else:
                 error_message = 'Email или пароль не верны!'
         else:
